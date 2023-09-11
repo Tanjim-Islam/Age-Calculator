2dce920a73e6555b62b50f4e8a7ce20466c93c07
Tanjim Riju <tanjim.islam.riju@g.bracu.ac.bd>
2023-09-12 05:17:31 +0600

    third

--- src/components/AgeResult.jsx
+++ src/components/AgeResult.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const AgeResult = ({ age }) => {
   return (
-    <h4 className="mt-10 underline text-2xl font-bold dark:text-white">
+    <h4 className="mt-10 underline text-sm sm:text-2xl font-bold dark:text-white break-words">
       You are {age.years} years, {age.months} months, and {age.days} days old.
     </h4>
   );
